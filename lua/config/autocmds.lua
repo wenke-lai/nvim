d3c1118a5a09acf8a54683e3b1276f7d020d4cf5
wenke.lai <wenke.studio@gmail.com>
2024-12-17 23:07:44 +0800

    feat: add a auto-cmd to find python venv for python project

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,42 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- check if python project
+    local python_files = {
+      "requirements.txt",
+      "pyproject.toml",
+    }
+
+    local is_python_project = false
+    for _, file in ipairs(python_files) do
+      if vim.fn.findfile(file, vim.fn.getcwd() .. ";") ~= "" then
+        is_python_project = true
+        break
+      end
+    end
+
+    if is_python_project then
+      -- source .venv/bin/activate
+      local venv_dir = vim.fn.finddir(".venv", vim.fn.getcwd() .. ";")
+      if venv_dir ~= "" then
+        local venv_path = vim.fn.fnamemodify(venv_dir, ":p:h")
+        local python_path = venv_path .. "/.venv/bin/python"
+
+        vim.env.VIRTUAL_ENV = venv_path .. "/.venv"
+        vim.env.PATH = venv_path .. "/.venv/bin:" .. vim.env.PATH
+
+        -- update LSP config
+        if vim.fn.filereadable(python_path) == 1 then
+          require("lspconfig").pyright.setup({
+            on_init = function(client)
+              client.config.settings.python.pythonPath = python_path
+            end,
+          })
+        end
+      end
+    end
+  end,
+})
