2dc28dac14d111737eb569dfe953395ab30e27f4
wenke.lai <wenke.studio@gmail.com>
2025-08-11 17:44:10 +0800

    feat(keymaps): add Neo-tree and window management keymaps
      Add keymaps for Neo-tree file explorer (<leader>e, <leader>o) and quick quit all windows (<leader>qq)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,9 @@
 
 vim.keymap.set("i", "kj", "<Esc>", { noremap = false })
+
+-- Neo-tree keymaps
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neo-tree" })
+vim.keymap.set("n", "<leader>o", "<cmd>Neotree focus<cr>", { desc = "Focus Neo-tree" })
+
+-- Window management
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all windows" })
