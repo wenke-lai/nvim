e085be5162601db276467e4e4e7807d97b884c13
wenke.lai <wenke.studio@gmail.com>
2025-08-12 00:15:19 +0800

    feat(keymaps): add save keybinding and improve quit behavior
      - Add <leader>s for quick file saving
      - Change <leader>qq to save all files before quitting (wqa)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,5 +5,8 @@ vim.keymap.set("i", "kj", "<Esc>", { noremap = false })
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neo-tree" })
 vim.keymap.set("n", "<leader>o", "<cmd>Neotree focus<cr>", { desc = "Focus Neo-tree" })
 
+-- File operations
+vim.keymap.set("n", "<leader>s", "<cmd>w<cr>", { desc = "Save file" })
+
 -- Window management
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all windows" })
+vim.keymap.set("n", "<leader>qq", "<cmd>wqa<cr>", { desc = "Save all and quit" })
