5fd07627edeaae9bf3a5f8ddc9b3e387b04d2208
wenke.lai <wenke.studio@gmail.com>
2025-12-09 16:16:55 +0800

    feat(snacks): configure explorer width to 25

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              width = 25, -- 設定 explorer 寬度
+              min_width = 25,
+            },
+          },
+        },
+      },
+    },
+  },
+}
