48efa909d7259151ee076bdd922f40e6ba9c502a
wenke.lai <wenke.studio@gmail.com>
2026-01-15 13:10:15 +0800

    feat: show hidden files without packages folder

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,7 +3,18 @@ return {
   opts = {
     picker = {
       sources = {
+        files = {
+          hidden = true,
+          exclude = {
+            "**/node_modules/**",
+            "**/.svelte-kit/**",
+          },
+        },
         explorer = {
+          hidden = true,
+          include = {
+            ".env",
+          },
           layout = {
             layout = {
               width = 25, -- 設定 explorer 寬度
